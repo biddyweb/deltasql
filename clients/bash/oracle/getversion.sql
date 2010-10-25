0b2c125c9f92da21d20fc606a2a752edf86bac74
dangermaus <dangermaus@users.sourceforge.net>
2010-10-25 20:51:58 +0200

    executescript can generate rawversion

--- clients/bash/oracle/getversion.sql
+++ clients/bash/oracle/getversion.sql
@@ -1,2 +1,5 @@
+SET ECHO OFF NEWP 0 SPA 0 PAGES 0 FEED OFF HEAD OFF TRIMS ON TAB OFF
+SPOOL rawversion.txt
 select versionnr from TBSYNCHRONIZE where versionnr = (select max(versionnr) from TBSYNCHRONIZE);
-
+SPOOL OFF
+quit
