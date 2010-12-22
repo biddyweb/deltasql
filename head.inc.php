6a6fa2afc382d75d77e2df3f6f62cd958871bae8
dangermaus <dangermaus@users.sourceforge.net>
2010-12-22 12:17:05 +0100

    fixed the rough way head.inc.php was implemented

--- head.inc.php
+++ head.inc.php
@@ -1,10 +1,13 @@
 <?php
 include_once ("utils/constants.inc.php");
 if (file_exists($configurationfile)) include_once ("conf/config.inc.php");
-if ($enterprise_edition==true) {?> 
+if ($enterprise_edition==true) {
+echo " 
     <center>
-      <a href="index.php"><img src="<?echo($enterprise_logo);?>"alt="logo" border=0></a>
+      <a href=\"index.php\"><img src=\"$enterprise_logo\" alt=\"logo\" border=0></a>
     </center>
-<?} else { ?>
-    <a href="index.php"><img src="pictures/deltasql.png" alt="logo" border=0></a>
-<?}?>
+";
+} else { 
+    echo "<a href=\"index.php\"><img src=\"pictures/deltasql.png\" alt=\"logo\" border=0></a>";
+}
+?>
