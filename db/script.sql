b029ba1aaf430afc67dbb66a12d02e34cfbac1bf
dangermaus <dangermaus@sourceforge.net>
2011-02-26 18:48:56 +0100

    added table to track changes of branches

--- db/script.sql
+++ db/script.sql
@@ -239,3 +239,12 @@ CREATE TABLE `tbphonetranscript` (
   `nbsb` int(11) NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=5 ;
+
+CREATE TABLE `tbscriptbranchchangelog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `script_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `script_id` (`script_id`,`branch_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+
