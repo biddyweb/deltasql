e51a65db273e081e3284188b8fcee3065af99148
dangermaus <dangermaus@users.sourceforge.net>
2011-03-03 14:13:04 +0100

    list changelog shows checkboxes to retrieve diffs

--- list_changelog.php
+++ list_changelog.php
@@ -42,9 +42,12 @@ $query="SELECT * from tbscriptchangelog where script_id=$scriptid ORDER BY id DE
 $result=mysql_query($query);  
 $num=mysql_numrows($result); 
 
+echo "<form action=\"get_diffs_changelog.php\" method=\"post\">";
 echo "
 <table border=\"1\">
 <tr>
+<th></th>
+<th></th>
 <th>description</th>
 <th>update user</th>
 <th>create dt</th>
@@ -54,6 +57,7 @@ echo "
 </tr>";
 echo "
 <tr>
+<td></td><td><input type=\"radio\" name=\"todiff\" value=\"latest\" checked></td>
 <td><b>Latest revision</b></td>
 <td>$upuser</td>
 <td>$updt</td>
@@ -85,9 +89,14 @@ if (strlen($comments)>10) {
 if ($update_user=="") { 
   $description = "<b>Initial revision</b>";
   $update_user=$author;
-} else $description = "";
+  $diffcells = "<td><input type=\"radio\" name=\"fromdiff\" value=\"$id\" checked></td><td></td>";
+} else {
+  $description = "";
+  $diffcells = "<td><input type=\"radio\" name=\"fromdiff\" value=\"$id\"></td><td><input type=\"radio\" name=\"todiff\" value=\"$id\"></td>";
+}
 echo "
 <tr>
+$diffcells
 <td>$description</td>
 <td>$update_user</td>
 <td>$create_dt</td>
@@ -99,8 +108,9 @@ echo "
 }
 ?>
 </table>
+<input type="Submit" value="Get diffs between revisions">
+</form>
 <br>
-
 <a href="list_scripts.php">Back to List scripts</a>
 </body>
 </html>
