0042a2d9cae4d4cf10e46ac13b69dae075e71454
dangermaus <dangermaus@users.sourceforge.net>
2011-08-22 13:29:17 +0200

    added deltaclient to table on initial page

--- download_clients_table.inc.php
+++ download_clients_table.inc.php
@@ -6,7 +6,10 @@ to try out one of the clients that connect to this server and which integrate in
 <center>
 <table border="1">
 <tr><th>client</th><th>environment</th><th>description</th><th>version</th><th>actions</th></tr>
-<tr><td><b>ant-client</b></td><td>Eclipse (Ant)</td><td>Lightweight, retrieves version from deltasql server and shows synchronization script (for MySQL, PostgreSQL, Oracle, MS SQL Server and Sybase). JDBC driver not included.</td><td><?php echo "$antclient_version"; ?></td><td><a href="clients/java/ant-client.zip"><center>download...</center></a></td></tr>
+<tr><td><b>deltaclient</b></td><td>Windows (Freepascal)</td><td>Multipurpose deltasql client written in Freepascal for Windows (XP, Vista, 7).
+ It directly retrieves available projects and branches from server. It copies the generated script to clipboard and shows it in the editor of choice. 
+ Source could be compiled also for other architectures.</td><td><?php echo "$deltaclient_version"; ?></td><td><a href="clients/freepascal/deltaclient.zip"><center>download...</center></a></td></tr>
+<tr><td><b>ant-client</b></td><td>Eclipse (Ant)</td><td>Lightweight, retrieves version directly from db schema and shows synchronization script from deltasql server (for MySQL, PostgreSQL, Oracle, MS SQL Server and Sybase). JDBC driver not included.</td><td><?php echo "$antclient_version"; ?></td><td><a href="clients/java/ant-client.zip"><center>download...</center></a></td></tr>
 <tr><td><b>bash client</b></d><td>GNU/Linux (bash)</td><td>This Bash script can be 
 used to achieve continouus integration for
  database schemas on a Linux server if scheduled with a cron job (for 
@@ -14,7 +17,7 @@ MySQL, PostgreSQL and Oracle).</td><td><?php echo
 "$bashclient_version" 
 ?></td><td><center><a 
 href="clients/bash_client.tar.gz">download...</a></center></td></tr>
-<tr><td><b>dbredactor</b></td><td>Eclipse (Java, Ant)</td><td>Retrieves version from deltasql server and shows synchronization script (for MySQL, PostgreSQL, Oracle, MS SQL Server and Sybase). JDBC driver included.</td><td><?php echo "$dbredactor_version"; ?></td><td><a href="clients/java/dbredactor.zip"><center>download...</center></a></td></tr>
+<tr><td><b>dbredactor</b></td><td>Eclipse (Java, Ant)</td><td>Retrieves version directly from db schema and shows synchronization script from deltasql server (for MySQL, PostgreSQL, Oracle, MS SQL Server and Sybase). JDBC driver included.</td><td><?php echo "$dbredactor_version"; ?></td><td><a href="clients/java/dbredactor.zip"><center>download...</center></a></td></tr>
 <tr><td><b>latest scripts</b></td><td>Google Gadget</td><td>Shows latest scripts on your server on iGoogle start page.</td><td><?php echo "$gadget_version"; ?></td><td><a href="http://www.google.ch/ig/adde?hl=en&moduleurl=http://www.gpu-grid.net/deltasql/deltasql_google_gadget.xml&source=imag" target=_blank><img src="pictures/add_google_gadget.gif" border="0" /></a>
 </td></tr>
 </table>
