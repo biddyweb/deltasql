dec33a1dc1daec0739c74d171b2ccdd2d9bd46fd
dangermouse <tiziano.mengotti@gmail.com>
2012-01-26 09:53:43 +0100

    script deletion also triggers email notification

--- delete_script.php
+++ delete_script.php
@@ -9,6 +9,8 @@
 include("head.inc.php");
 include("conf/config.inc.php");
 include("utils/utils.inc.php");
+include("utils/constants.inc.php");
+include("utils/sendmail.inc.php");
 include("changelog.inc.php");
 
 $rights = $_SESSION["rights"];
@@ -24,6 +26,21 @@ mysql_connect($dbserver, $username, $password);
 
 // store a backup copy of the script
 copy_script_to_changelog($scriptid);
+
+//email notification
+// 4. Email notification
+if ($emails_enable) {
+  // retrieve script version
+  $query22="SELECT * FROM tbscript WHERE id=$scriptid;";
+  mysql_query($query22);
+  $result22=mysql_query($query22);
+  $version=mysql_result($result22,0,"versionnr");
+  $body=mysql_result($result22,0,"code");;
+  
+  $subject="$emails_subject_identifier $version DELETED by $user";
+  notify_users_with_email($sendmail_command,$deltasql_path,$emails_sender, $subject, $body);
+}
+
 
 $query2="DELETE FROM tbscriptbranch where script_id=$scriptid;";
 mysql_query($query2);
