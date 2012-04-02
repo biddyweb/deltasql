0a5488fe5301506e6fc479c817642b33d02f9b3f
dangermouse <tiziano.mengotti@gmail.com>
2012-04-02 16:08:02 +0200

    homepage update

--- web/index.php
+++ web/index.php
@@ -82,8 +82,9 @@ Switzerland and India
 and is popular in Japan and South Korea. In some companies it manages 
 more than 
 2000 scripts and more than 10 projects.</li>
-<li>Typically used for large J2EE/Oracle software architectures which are partially customized to the customer's wishes.</li>
+<li>Typically used for large J2EE/Oracle or J2EE/mySQL software architectures which are partially customized to the customer's wishes.</li>
 <li>Ability to send email notifications to users who would like to work without deltasql.</li>
+<li><a href="deltasql/server_stats.php">Charting</a> features to monitor how deltasql improves over time.</li>
 <li>Lightweight, fast and reliable :-)</li>
 </ul>
 <?php
