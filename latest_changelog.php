689d198e5945a844f56b13060e6a92c3dfd05b94
dangermouse <tiziano.mengotti@gmail.com>
2012-04-04 09:11:32 +0200

    minor changes in latest changelog

--- latest_changelog.php
+++ latest_changelog.php
@@ -21,7 +21,7 @@ include("conf/config.inc.php");
  ';
  }
 
-$version=$_GET['version'];
+if (isset($_GET['version'])) $version=$_GET['version']; else $version="unknown";
 echo "<h2>Latest ChangeLog for $deltasql_version</h2>";
 if ($version==$deltasql_version) {
   echo "<i>$deltasql_version is currently the latest available release from www.deltasql.org.</i>";
@@ -29,11 +29,38 @@ if ($version==$deltasql_version) {
   echo "<i>Your deltasql server version is $version. A newer version $deltasql_version is available from www.deltasql.org. Read below for the latest changes...</i>";
 }
 echo "<hr>";
-
 ?>
 
 <pre>
 <?php include("docs/ChangeLog.txt"); ?>
 </pre>
+
+<?php
+if (($dns_name=="http://www.deltasql.org/deltasql") && ($version!="unknown")) {
+  $ip        = $_SERVER['REMOTE_ADDR'];
+  $port      = $_SERVER['REMOTE_PORT'];
+  $referrer  = $_SERVER['HTTP_REFERER'];
+  $useragent = $_SERVER['HTTP_USER_AGENT'];
+  mysql_connect($dbserver, $username, $password);
+  @mysql_select_db($database) or die("Unable to select database");
+  $query="INSERT INTO tbqos (id, deltasql_version, ip, port, referrer, useragent, create_dt) VALUES('', '$version', '$ip', '$port', '$referrer', '$useragent', NOW());";
+  mysql_query($query);
+  mysql_close();
+}
+
+/*
+CREATE TABLE IF NOT EXISTS `tbqos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deltasql_version` varchar(32) COLLATE latin1_general_ci DEFAULT NULL,
+  `ip` varchar(32) COLLATE latin1_general_ci DEFAULT NULL,
+  `port` varchar(5) COLLATE latin1_general_ci DEFAULT NULL,
+  `referrer` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+  `useragent` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+  `create_dt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=5 ;
+*/
+
+?>
 </body>
 </html>
