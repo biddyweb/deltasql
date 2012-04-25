c17efceb3f99a73883f1b181d1b2b2c4dd762b7a
dangermouse <tiziano.mengotti@gmail.com>
2012-04-25 18:25:15 +0200

    python client retrieves version number

--- clients/python/pyclient/pyclient.py
+++ clients/python/pyclient/pyclient.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 import MySQLdb
 
-db=MySQLdb.connect("localhost","deltauser","deltapass","deltasql")
+db=MySQLdb.connect("localhost","deltasqluser","deltapass","deltasql")
 
 c=db.cursor()
-c.execute('SELECT * FROM tbsynchronize')
+c.execute('select versionnr from tbsynchronize where versionnr = (select max(versionnr) from tbsynchronize);')
+ver=c.fetchall()
 
-c.fetchall()
+print ver
 
 db.close()
