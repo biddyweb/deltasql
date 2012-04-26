6d25ec969e5c594183841db04e18077850e64751
dangermaus <dangermaus@users.sourceforge.net>
2012-04-26 17:33:56 +0200

    some problems with urllib

--- clients/python/pyclient/pyclient.py
+++ clients/python/pyclient/pyclient.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python
 import MySQLdb
 import SimpleConfigParser
+import urllib
 
+# opening configuration file
 cp = SimpleConfigParser.SimpleConfigParser()
 cp.read('config.ini')
 
-"""
-print 'getoptionslist():', cp.getoptionslist()
-for option in cp.getoptionslist():
-    print "getoption('%s') = '%s'" % (option, cp.getoption(option))
-print "hasoption('wrongname') =", cp.hasoption('wrongname')
-"""
-
+# First we retrieve the current version from the database schema
 db=MySQLdb.connect(cp.getoption('host'),cp.getoption('username'),
                    cp.getoption('password'),cp.getoption('database'))
 
 c=db.cursor()
 c.execute('select versionnr from tbsynchronize where versionnr = (select max(versionnr) from tbsynchronize);')
 ver=c.fetchone()
-
+db.close()
 versionnr= ver[0]
 print "Database schema is currently at version "+str(versionnr)
 
-db.close()
+# We check the current version on the external deltasql server
+
+wwwurl = cp.getoption('url')+'/dbsync_automated_currentversion.php?project='+cp.getoption('project')
+print wwwurl
+
+serverUrl = urllib.urlopen(wwwurl)
+properties= serverUrl.read()
+print properties
+
