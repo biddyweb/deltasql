69507c40093be0ac8f2eedbe559f39ce566c189b
dangermaus <dangermaus@users.sourceforge.net>
2012-04-27 10:14:09 +0200

    python script downloads version info from server :-)

--- clients/python/pyclient/pyclient.py
+++ clients/python/pyclient/pyclient.py
@@ -16,14 +16,23 @@ c.execute('select versionnr from tbsynchronize where versionnr = (select max(ver
 ver=c.fetchone()
 db.close()
 versionnr= ver[0]
-print "Database schema is currently at version "+str(versionnr)
+print "Database schema is currently at version "+str(versionnr)+"."
 
 # We check the current version on the external deltasql server
-
 wwwurl = cp.getoption('url')+'/dbsync_automated_currentversion.php?project='+cp.getoption('project')
-print wwwurl
-
 serverUrl = urllib.urlopen(wwwurl)
-properties= serverUrl.read()
-print properties
+f = open('project.properties', 'wb')
+f.write(serverUrl.read())
+f.close()
+pp = SimpleConfigParser.SimpleConfigParser()
+pp.read('project.properties')
+srvversionnr=pp.getoption('project.version')
+print "On deltasql server, project "+cp.getoption("project")+" is at version "+str(srvversionnr)+"."
+if (srvversionnr<=versionnr):
+	print "Nothing to do for the moment. Exiting..."
+else:
+	print "Downloading synchronization script from server..."
+
+
+
 
