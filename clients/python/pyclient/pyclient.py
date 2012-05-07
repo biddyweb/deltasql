b2d2fc61726bd6513c360069f2e49a7f3472e46a
dangermaus <dangermaus@users.sourceforge.net>
2012-05-07 10:31:32 +0200

    downloading synchronization script

--- clients/python/pyclient/pyclient.py
+++ clients/python/pyclient/pyclient.py
@@ -2,6 +2,12 @@
 import MySQLdb
 import SimpleConfigParser
 import urllib
+import os
+
+# some initial clenaup steps
+if os.path.exists('project.properties'): os.unlink('project.properties')
+if os.path.exists('script.sql'): os.unlink('script.sql')
+
 
 # opening configuration file
 cp = SimpleConfigParser.SimpleConfigParser()
@@ -19,19 +25,27 @@ versionnr= ver[0]
 print "Database schema is currently at version "+str(versionnr)+"."
 
 # We check the current version on the external deltasql server
-wwwurl = cp.getoption('url')+'/dbsync_automated_currentversion.php?project='+cp.getoption('project')
-serverUrl = urllib.urlopen(wwwurl)
+versionurl = cp.getoption('url')+'/dbsync_automated_currentversion.php?project='+cp.getoption('project')
+versionpage = urllib.urlopen(versionurl)
 f = open('project.properties', 'wb')
-f.write(serverUrl.read())
+f.write(versionpage.read())
 f.close()
 pp = SimpleConfigParser.SimpleConfigParser()
 pp.read('project.properties')
 srvversionnr=pp.getoption('project.version')
+if os.path.exists('project.properties'): os.unlink('project.properties')
+
 print "On deltasql server, project "+cp.getoption("project")+" is at version "+str(srvversionnr)+"."
 if (srvversionnr<=versionnr):
 	print "Nothing to do for the moment. Exiting..."
 else:
 	print "Downloading synchronization script from server..."
+        scripturl = cp.getoption('url')+'/dbsync_automated_update.php?project='+cp.getoption('project')+'&version='+str(versionnr)+'&frombranch='+cp.getoption('frombranch')+'&tobranch='+cp.getoption('tobranch')
+	scriptpage = urllib.urlopen(scripturl)
+        f = open('script.sql', 'wb')
+        f.write(scriptpage.read())
+        f.close()
+	print('Synchronization script downloaded :-)');
 
 
 
