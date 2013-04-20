77c00417fa64a189ef0e90b4a2a12a3d924cad03
dangermaus <dangermaus@users.sourceforge.net>
2013-04-20 22:13:54 +0200

    doc

--- web/menu.inc.php
+++ web/menu.inc.php
@@ -5,8 +5,8 @@
 <li><a href="index.php" class="menu">Introduction</a></li>
 <li><a href="features.php" class="menu">Features</a></li>
 <li><a href="faq.php" class="menu">F.A.Q.</a></li>
-<li><a href="deltasql/" class="menu">Online Demo</a></li>
 <li><a href="manual.php" class="menu">Manual</a></li>
+<li><a href="deltasql/" class="menu">Online Demo ...</a></li>
 <li><a href="http://sourceforge.net/projects/deltasql/files/" class="menu">Download ...</a></li>
 <li><a href="http://sourceforge.net/projects/deltasql/" class="menu">Project ...</a></li>
 <li><a href="http://sourceforge.net/p/deltasql/discussion/" class="menu">Forum ...</a></li>
