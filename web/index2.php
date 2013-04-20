0e32e324c302df7d33d7dc6c04ac7cc775648387
dangermaus <dangermaus@users.sourceforge.net>
2013-04-20 19:18:14 +0200

    doc

--- web/index2.php
+++ web/index2.php
@@ -1,24 +1,13 @@
 <html>
 <head>
-<title>deltasql - Database Evolution under Control</title>
+<title>deltasql - A software to synchronize Database Schemas</title>
 </head>
 <body>
-<div align="top">
-<h3>deltasql - A software to synchronize database schemas</h3>
-</div>
-<div align="left">
-<ul>
-<li><a href="deltasql/manual.php" class="menu">Manual</a></li>
-<li><a href="deltasql/faq.php" class="menu">FAQ</a></li>
-<li><a href="deltasql/" class="menu">Test deltasql online</a></li>
-<li><a href="http://sourceforge.net/projects/deltasql/files/" class="menu">Download deltasql</a></li>
-<li><a href="http://sourceforge.net/projects/deltasql/" class="menu">Project 
-page</a></li>
-<li><a href="http://sourceforge.net/p/deltasql/discussion/" class="menu">Forum</a></li>
-</ul>
-</div>
-
+<php include("top.php"); ?>
+<php include("menu.php"); ?>
 <div>
+Test Frame
 </div>
+<php include("bottom.php"); ?>
 </body>
 </html>
---
+++ web/top.php
@@ -0,0 +1,3 @@
+<div align="top">
+<h3>deltasql - Database Evolution under Control</h3>
+</div>
