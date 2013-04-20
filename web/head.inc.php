ecbf4fe0072935d08e71ab77e4e4e66461d9fd1d
dangermaus <dangermaus@users.sourceforge.net>
2013-04-20 22:24:51 +0200

    doc

--- web/head.inc.php
+++ web/head.inc.php
@@ -1,7 +1,4 @@
 <?php 
-echo "I am here"; 
 include("top.inc.php");
-echo "I am here II"; 
 include("menu.inc.php"); 
-echo "I am here III"; 
 ?>
--- web/menu.inc.php
+++ web/menu.inc.php
@@ -6,7 +6,7 @@
 <li><a href="features.php" class="menu">Features</a></li>
 <li><a href="faq.php" class="menu">F.A.Q.</a></li>
 <li><a href="manual.php" class="menu">Manual</a></li>
-<li><a href="deltasql/" class="menu">Online Demo ...</a></li>
+<li><a href="deltasql/" class="menu">Online Demo</a></li>
 <li><a href="http://sourceforge.net/projects/deltasql/files/" class="menu">Download ...</a></li>
 <li><a href="http://sourceforge.net/projects/deltasql/" class="menu">Project ...</a></li>
 <li><a href="http://sourceforge.net/p/deltasql/discussion/" class="menu">Forum ...</a></li>
