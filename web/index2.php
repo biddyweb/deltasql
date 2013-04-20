7201413286ad7c3a320192d4e7f1b864c29c53a4
dangermaus <dangermaus@users.sourceforge.net>
2013-04-20 19:00:17 +0200

    testing new homepage skeleton

---
+++ web/index2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>deltasql - Database Evolution under Control</title>
+</head>
+<body>
+<div align="right">
+<ul>
+<li><a href="deltasql/manual.php" class="menu">Manual</a></li>
+<li><a href="deltasql/faq.php" class="menu">FAQ</a></li>
+<li><a href="deltasql/" class="menu">Test deltasql online</a></li>
+<li><a href="http://sourceforge.net/projects/deltasql/files/" class="menu">Download deltasql</a></li>
+<li><a href="http://sourceforge.net/projects/deltasql/" class="menu">Project 
+page</a></li>
+<li><a href="http://sourceforge.net/p/deltasql/discussion/" class="menu">Forum</a></li>
+</ul>
+</div>
+</body>
+</html>
