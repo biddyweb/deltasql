d6af9521c0be33c83b3d85a90654970ba9fbe7a8
dangermaus <dangermaus@sourceforge.net>
2013-04-21 14:36:26 +0200

    now the webpage is in a good shape :-)

--- web/menu.inc.php
+++ web/menu.inc.php
@@ -7,6 +7,7 @@
 <li><a href="faq.php" class="menu">F.A.Q.</a></li>
 <li><a href="tryonline.php" class="menu">Try Online</a></li>
 <li><a href="download.php" class="menu">Download</a></li>
+<li><a href="tutorials.php" class="menu">Tutorials</a></li>
 <li><a href="manual.php" class="menu">Manual</a></li>
 <li><a href="http://sourceforge.net/p/deltasql/discussion/" class="menu" target="blank_">Forum...</a></li>
 <li><a href="http://sourceforge.net/projects/deltasql/" class="menu" target="blank_">Project...</a></li>
---
+++ web/tutorials.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>deltasql - A software to synchronize Database Schemas</title>
+<?php include("meta.inc.php"); ?>
+</head>
+<body>
+<?php include("top.inc.php"); ?>
+<?php include("menu.inc.php"); ?>
+
+
+
+<center>
+<i>
+<p>
+"... ehm, no testimonial here, as both the video quality and the English language is what it is. But we are sure, you'll do the best out of it ;-)"
+<br>
+</center>
+
+
+<h2><a href="http://sourceforge.net/projects/deltasql/files/">Tutorials</a></h2>
+
+<a href="http://sourceforge.net/projects/deltasql/files/"><img src="pictures/movie.jpg" border="0"></a>
+
+<p>
+Tutorials are videos with length between 3 and 15 minutes, kindly provided through <a href="http://sourceforge.net">Sourceforge</a> mirrors. Please click here to visit 
+<a href="http://sourceforge.net/projects/deltasql/files/">the external download site</a>.
+</p>
+
+<?php include("bottom.inc.php"); ?>
+</body>
+</html>
