4d295df22a6ed102a5333124a494385d6d4b4735
dangermaus <dangermaus@sourceforge.net>
2013-04-21 14:26:37 +0200

    added tryonline page

--- web/menu.inc.php
+++ web/menu.inc.php
@@ -5,8 +5,8 @@
 <li><a href="index.php" class="menu">Introduction</a></li>
 <li><a href="features.php" class="menu">Features</a></li>
 <li><a href="faq.php" class="menu">F.A.Q.</a></li>
-<li><a href="deltasql/" class="menu" target="blank_">Try Online</a></li>
-<li><a href="download.php" class="menu" target="blank_">Download</a></li>
+<li><a href="tryonline.php" class="menu">Try Online</a></li>
+<li><a href="download.php" class="menu">Download</a></li>
 <li><a href="manual.php" class="menu">Manual</a></li>
 <li><a href="http://sourceforge.net/p/deltasql/discussion/" class="menu" target="blank_">Forum...</a></li>
 <li><a href="http://sourceforge.net/projects/deltasql/" class="menu" target="blank_">Project...</a></li>
