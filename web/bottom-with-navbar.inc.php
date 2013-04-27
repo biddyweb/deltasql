c4d2d023f5d5fc09792dbb222bd684a53339f1dc
dangermaus <dangermaus@sourceforge.net>
2013-04-27 14:15:33 +0200

    doc

--- web/bottom-with-navbar.inc.php
+++ web/bottom-with-navbar.inc.php
@@ -4,7 +4,7 @@
 <td></td>
 <td>
 <hr>
-<a href="index.php">Back to Introduction</a><br>";
+<a href="index.php">Back to Introduction</a><br>
 <center>
 <h6>Copyright (c) 2007-2013 <a href="http://sourceforge.net/projects/deltasql/">the deltasql team</a>. Source code of this software is under <a href="docs/GPL_license.txt">GPL license</a>. deltasql is kinldy hosted by <a href="http://www.sourceforge.net">Sourceforge</a>.</h6><br>
 <a href="http://www.sourceforge.net">
