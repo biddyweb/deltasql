d58eb4c799b79021632b40de6ae541ed94a2518b
dangermouse <dangermaus@users.sf.net>
2013-06-24 13:40:24 +0200

    ajax query behaves correctly

--- ajax/get_branches_with_module_and_script.php
+++ ajax/get_branches_with_module_and_script.php
@@ -6,24 +6,32 @@ mysql_connect($dbserver, $username, $password);
 @mysql_select_db($database) or die("Unable to select database");
 
     $moduleid = trim($_REQUEST['moduleid']);
-    $scriptd = trim($_REQUEST['scriptid']);
+    $scriptid = trim($_REQUEST['scriptid']);
     if (($moduleid != "") && ($scriptid != ""))
 	{
         $headid  = retrieve_head_id();
         $str =  "$headid:";
         // checking if the script belongs to head
-        $checkhead="SELECT * from tbmoduleproject WHERE script_id="+$scriptid+" AND ";
+        $checkhead="SELECT count(*) from tbscriptbranch WHERE script_id=$scriptid AND branch_id=$headid;";
+        //echo "$checkhead\n";
+        $checkres=mysql_query($checkhead);
+        $checkcount=mysql_result($checkres, 0, "count(*)");
         
-        $str = $str . "HEAD###";
+        $str = $str . $checkcount . ":HEAD###";
 	
         $sql = sprintf("SELECT b.id, b.name FROM tbbranch b, tbmoduleproject mp WHERE b.visible=1 and b.istag=0 and b.project_id=mp.project_id and mp.module_id=%d order by name asc", $moduleid);
-		
         $rs = mysql_query($sql);
 		if (mysql_num_rows($rs) > 0) 
 		{
 			while($row = mysql_fetch_assoc($rs))
 			{
-				$str .=  $row['id'].":".$row['name']."###";
+                // checking if the script belongs to this branch
+                $checkbranch="SELECT count(*) from tbscriptbranch WHERE script_id=$scriptid AND branch_id=".$row['id'];
+                //echo "$checkbranch\n";
+                $checkbres=mysql_query($checkbranch);
+                $checkbcount=mysql_result($checkbres, 0, "count(*)");
+				
+                $str .=  $row['id'].":".$checkbcount.":".$row['name']."###";
 			}
 		}
         echo $str = substr($str,0,-3);
