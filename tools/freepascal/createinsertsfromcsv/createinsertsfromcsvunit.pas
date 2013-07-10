cbb8e7cdd544b7312e0e38111102986723eebfd6
dangermouse <dangermaus@users.sf.net>
2013-07-10 09:23:56 +0200

    GUI created

--- tools/freepascal/createinsertsfromcsv/createinsertsfromcsvunit.pas
+++ tools/freepascal/createinsertsfromcsv/createinsertsfromcsvunit.pas
@@ -5,10 +5,25 @@ unit createinsertsfromcsvunit;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TfrmCreateInserts }
+
   TfrmCreateInserts = class(TForm)
+    btnSelectFileName: TButton;
+    btnGenerate: TButton;
+    cbHeader: TCheckBox;
+    edtFilename: TEdit;
+    lblFilename: TLabel;
+    lblOutput: TLabel;
+    lblInfo: TLabel;
+    lblOutputLink: TLabel;
+    dlgOpen: TOpenDialog;
+    procedure btnGenerateClick(Sender: TObject);
+    procedure btnSelectFileNameClick(Sender: TObject);
+    procedure lblOutputLinkClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -21,6 +36,23 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TfrmCreateInserts }
+
+procedure TfrmCreateInserts.btnGenerateClick(Sender: TObject);
+begin
+    //
+end;
+
+procedure TfrmCreateInserts.btnSelectFileNameClick(Sender: TObject);
+begin
+  //
+end;
+
+procedure TfrmCreateInserts.lblOutputLinkClick(Sender: TObject);
+begin
+  //
+end;
 
 end.
 
