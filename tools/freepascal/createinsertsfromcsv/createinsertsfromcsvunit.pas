2bb0fe4bdcded8f69aaeb69774b0b22071a6718f
dangermouse <dangermaus@users.sf.net>
2013-07-10 11:11:28 +0200

    insert statement created

--- tools/freepascal/createinsertsfromcsv/createinsertsfromcsvunit.pas
+++ tools/freepascal/createinsertsfromcsv/createinsertsfromcsvunit.pas
@@ -8,9 +8,6 @@ uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
-
-  { TfrmCreateInserts }
-
   TfrmCreateInserts = class(TForm)
     btnSelectFileName: TButton;
     btnGenerate: TButton;
@@ -27,8 +24,11 @@ type
     procedure btnSelectFileNameClick(Sender: TObject);
     procedure lblOutputLinkClick(Sender: TObject);
   private
-    outputFile : AnsiString;
+    outputFile,
+    tableName,
+    insertStatement : AnsiString;
     procedure generateInserts;
+    procedure constructInsertStatement(header : AnsiString);
   end;
 
 var
@@ -37,6 +37,24 @@ var
 implementation
 
 {$R *.lfm}
+
+function ExtractParamLong(var S: AnsiString; Separator: string): AnsiString;
+var
+  i: Longint;
+begin
+  i := Pos(Separator, S);
+  if i > 0 then
+  begin
+    Result := Copy(S, 1, i - 1);
+    Delete(S, 1, i-1);
+    Delete(S, 1, length(Separator));
+  end
+  else
+  begin
+    Result := S;
+    S      := '';
+  end;
+end;
 
 procedure TfrmCreateInserts.btnSelectFileNameClick(Sender: TObject);
 begin
@@ -64,15 +82,28 @@ end;
 
 procedure TfrmCreateInserts.generateInserts;
 var F, G : TextFile;
+    str  : Ansistring;
+    firstLine : Boolean;
 begin
   outputFile := ChangeFileExt(edtFileName.Text, '.sql');
-  ShowMessage(outputFile);
+  //ShowMessage(outputFile);
 
+  firstLine := true;
   AssignFile(F, edtFileName.Text);
   AssignFile(G, outputFile);
   try
     Reset(F);
     Rewrite(G);
+
+    while not EOF(F) do
+        begin
+          Readln(F, str);
+          if firstLine then
+                begin
+                    constructInsertStatement(str);
+                    firstLine := false;
+                end;
+        end;
 
   finally
     CloseFile(F);
@@ -80,9 +111,36 @@ begin
   end;
 end;
 
+procedure TfrmCreateInserts.constructInsertStatement(header : AnsiString);
+var column : AnsiString;
+begin
+   tableName := ExtractFileName(changeFileExt(outputFile, ''));
+   //ShowMessage('Tablename is '+tableName);
+
+   if not cbHeader.Checked then
+         begin
+             insertStatement := 'INSERT INTO '+tableName+' VALUES(';
+         end
+   else
+         begin
+             insertStatement := 'INSERT INTO '+tableName+' (';
+             // we need to retrieve first the column names
+             column:=Trim(extractParamLong(header, edtSeparator.Text));
+             while column<>'' do
+                 begin
+                   insertStatement := insertStatement + column + ',';
+                   column:=Trim(extractParamLong(header, edtSeparator.Text));
+                 end;
+             Delete(insertStatement, length(insertStatement), 1);
+             insertStatement := insertStatement+') VALUES(';
+         end;
+   ShowMessage(insertStatement);
+end;
+
 procedure TfrmCreateInserts.lblOutputLinkClick(Sender: TObject);
 begin
-  //
+
+
 end;
 
 end.
