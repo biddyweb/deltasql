a2b57d4981e888ad4b87e8415e78c0afd809c317
dangermouse <dangermaus@users.sf.net>
2013-07-10 10:30:45 +0200

    added logic to select a csv file

--- tools/freepascal/createinsertsfromcsv/createinsertsfromcsvunit.pas
+++ tools/freepascal/createinsertsfromcsv/createinsertsfromcsvunit.pas
@@ -8,9 +8,6 @@ uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
-
-  { TfrmCreateInserts }
-
   TfrmCreateInserts = class(TForm)
     btnSelectFileName: TButton;
     btnGenerate: TButton;
@@ -25,9 +22,7 @@ type
     procedure btnSelectFileNameClick(Sender: TObject);
     procedure lblOutputLinkClick(Sender: TObject);
   private
-    { private declarations }
-  public
-    { public declarations }
+    procedure generateInserts;
   end;
 
 var
@@ -37,16 +32,30 @@ implementation
 
 {$R *.lfm}
 
-{ TfrmCreateInserts }
+procedure TfrmCreateInserts.btnSelectFileNameClick(Sender: TObject);
+begin
+  if dlgOpen.Execute then
+     begin
+          edtFileName.Text := dlgOpen.FileName;
+          edtFileName.Enabled := true;
+     end;
+end;
 
 procedure TfrmCreateInserts.btnGenerateClick(Sender: TObject);
 begin
-    //
+     if Trim(edtFileName.Text)<>'' then
+        begin
+            if FileExists(edtFileName.Text) then
+               generateInserts
+            else
+              ShowMessage('ERROR: could not locate the filename! ('+edtFileName.Text+')');
+        end
+     else ShowMessage('Please select an input filename first with "..."');
 end;
 
-procedure TfrmCreateInserts.btnSelectFileNameClick(Sender: TObject);
+
+procedure TfrmCreateInserts.generateInserts;
 begin
-  //
 end;
 
 procedure TfrmCreateInserts.lblOutputLinkClick(Sender: TObject);
