c27fbf21f4124fcbdad7626ce1e7eaae41a17572
dangermouse <dangermaus@users.sf.net>
2013-07-11 15:11:54 +0200

    GUI and validation ready. Now logic is missing

--- tools/freepascal/fromcsvdiffcreatesql/fromcsvdiffcreatesqlunit.pas
+++ tools/freepascal/fromcsvdiffcreatesql/fromcsvdiffcreatesqlunit.pas
@@ -45,6 +45,8 @@ type
     procedure fillCBPrimaryKey(filename : String);
     function  readMyHeader(myfilename : String) : AnsiString;
     function  getSeparator() : AnsiString;
+    function  checkForEqualHeader(myfilename : String) : Boolean;
+    procedure enableControls(value : Boolean);
   end;
 
 var
@@ -74,7 +76,44 @@ end;
 
 procedure TfromCSVtoSQL.btnGenerateSyncClick(Sender: TObject);
 begin
+  if Trim(edtTableName.Text)='' then
+         begin
+           ShowMessage('ERROR: please fill the tablename first!');
+           Exit;
+         end;
+  if Trim(cbPrimaryKey.Text)='' then
+         begin
+           ShowMessage('ERROR: please fill the primary key column first!');
+           Exit;
+         end;
+  if not FileExists(edtFileNameBefore.Text) then
+         begin
+           ShowMessage('ERROR: the first .csv file does not exist on the file system!');
+           Exit;
+         end;
+  if not FileExists(edtFileNameAfter.Text) then
+         begin
+           ShowMessage('ERROR: the second .csv file does not exist on the file system!');
+           Exit;
+         end;
 
+  enableControls(false);
+  // add here the logic that creates the sync script.
+  enableControls(true);
+end;
+
+procedure TfromCSVtoSQL.enableControls(value : Boolean);
+begin
+  edtFileNameBefore.Enabled := value;
+  edtFileNameAfter.Enabled := value;
+  btnSelectCSVFileBefore.Enabled := value;
+  btnSelectCSVFileAfter.Enabled := value;
+  edtTableName.Enabled := value;
+  rbSeparatorEdit.Enabled := value;
+  rbSeparatorTab.Enabled := value;
+  edtSeparator.Enabled := value;
+  cbPrimaryKey.Enabled := value;
+  btnGenerateSync.Enabled := value;
 end;
 
 procedure TfromCSVtoSQL.btnSelectCSVFileBeforeClick(Sender: TObject);
@@ -82,16 +121,34 @@ begin
   if dlgOpenCSV.Execute then
      begin
         edtFileNameBefore.Text := dlgOpenCSV.FileName;
-        fillCBPrimaryKey(edtFileNameAfter.Text);
+        fillCBPrimaryKey(edtFileNameBefore.Text);
      end;
 end;
 
 procedure TfromCSVtoSQL.btnSelectCSVFileAfterClick(Sender: TObject);
 begin
+  if Trim(edtFileNameBefore.Text)='' then
+     begin
+        ShowMessage('ERROR: please fill first the other .csv file!');
+        Exit;
+     end;
+
   if dlgOpenCSV.Execute then
      begin
         edtFileNameAfter.Text := dlgOpenCSV.FileName;
-        fillCBPrimaryKey(edtFileNameAfter.Text);
+        if Trim(edtFileNameAfter.Text) = Trim(edtFileNameBefore.Text) then
+           begin
+              ShowMessage('ERROR: you are using the same file!');
+              edtFileNameAfter.Text := '';
+              Exit;
+           end;
+
+        if not checkForEqualHeader(edtFileNameAfter.Text) then
+             begin
+                ShowMessage('ERROR: the two headers are different!');
+                edtFileNameAfter.Text := '';
+                Exit;
+             end;
      end;
 
 end;
@@ -100,6 +157,14 @@ end;
 function TfromCSVtoSQL.readMyHeader(myfilename : String) : AnsiString;
 var str : AnsiString;
 begin
+  Result := '';
+  if Trim(myFilename)='' then Exit;
+  if not FileExists(myfilename) then
+     begin
+        ShowMessage('ERROR: filename '+myfilename+' does not exist!');
+        Exit;
+     end;
+
   str := '';
   AssignFile(F, myfilename);
   try
@@ -109,7 +174,7 @@ begin
     CloseFile(F);
   end;
 
-  Result := Str;
+  Result := Trim(Str);
 end;
 
 procedure TfromCSVtoSQL.fillCBPrimaryKey(filename : String);
@@ -118,14 +183,12 @@ var header,
 begin
    cbPrimaryKey.Clear;
    header := readMyHeader(filename);
-   {
    column := Trim(ExtractParamLong(header, getSeparator()));
    while column<>'' do
          begin
            cbPrimaryKey.AddItem(column, nil);
            column := Trim(ExtractParamLong(header, getSeparator()));
          end;
-   }
 end;
 
 function TfromCSVtoSQL.getSeparator(): AnsiString;
@@ -137,6 +200,14 @@ begin
       Result := Chr(9)
   else
     ShowMessage('Internal error: undefined radio box');
+end;
+
+function TfromCSVtoSQL.checkForEqualHeader(myfilename : String) : Boolean;
+var headerFrom, HeaderTo : AnsiString;
+begin
+  headerFrom:=readMyHeader(edtFileNameBefore.Text);
+  headerTo:=readMyHeader(myFileName);
+  Result:= (headerFrom=headerTo)
 end;
 
 end.
