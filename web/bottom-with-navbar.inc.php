4389883f20f4e403cb77bfdba92d53102b2030d7
dangermaus <dangermaus@users.sourceforge.net>
2014-02-01 22:00:51 +0100

    updated copyrights to 2014

--- web/bottom-with-navbar.inc.php
+++ web/bottom-with-navbar.inc.php
@@ -6,7 +6,7 @@
 <hr>
 <a href="index.php">Back to Introduction</a><br>
 <center>
-<h6>Copyright (c) 2007-2013 <a href="http://sourceforge.net/projects/deltasql/">the deltasql team</a>. Source code of this software is under <a href="docs/GPL_license.txt">GPL license</a>. deltasql is kinldy hosted by <a href="http://www.sourceforge.net">Sourceforge</a>.</h6><br>
+<h6>Copyright (c) 2007-2014 <a href="http://sourceforge.net/projects/deltasql/">the deltasql team</a>. Source code of this software is under <a href="docs/GPL_license.txt">GPL license</a>. deltasql is kinldy hosted by <a href="http://www.sourceforge.net">Sourceforge</a>.</h6><br>
 <a href="http://www.sourceforge.net">
 <img src="deltasql/pictures/sourceforge-logo.gif" border="0" />
 </a>
